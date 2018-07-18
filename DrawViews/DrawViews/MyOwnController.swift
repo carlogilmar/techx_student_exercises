04ef9d2e791557a822a87041012dfeb8be55c3b1
carlogilmar <carlogilmar12@gmail.com>
2018-07-18 00:18:24 -0500

    Adding a header

--- DrawViews/DrawViews/MyOwnController.swift
+++ DrawViews/DrawViews/MyOwnController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class MyOwnController: UIViewController {
     
+    let headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        view.addSubview(headerView)
+        
+        // Create the anchors
+        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        
+        /* Ancho y largo van a 1*/
+        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.20).isActive = true
     }
     
 }
