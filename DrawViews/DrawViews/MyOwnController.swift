2ff208f64c16b2d17da2fdb959d5979d694a8ed4
carlogilmar <carlogilmar12@gmail.com>
2018-07-17 23:56:24 -0500

    Adding my own controller for reply the exercise

---
+++ DrawViews/DrawViews/MyOwnController.swift
@@ -0,0 +1,18 @@
+//
+//  MyOwnController.swift
+//  DrawViews
+//
+//  Created by Carlo Gilmar on 17/07/18.
+//  Copyright © 2018 Lets Build That App. All rights reserved.
+//
+
+import UIKit
+
+class MyOwnController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+}
