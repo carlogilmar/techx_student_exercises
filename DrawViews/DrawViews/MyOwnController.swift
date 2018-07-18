30643017a55250574180b5108ea01469b97cd5b6
carlogilmar <carlogilmar12@gmail.com>
2018-07-18 00:36:39 -0500

    Adding a label

--- DrawViews/DrawViews/MyOwnController.swift
+++ DrawViews/DrawViews/MyOwnController.swift
@@ -17,19 +17,35 @@ class MyOwnController: UIViewController {
         return view
     }()
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "HOLA PERROS"
+        label.textAlignment = .center
+        label.font = UIFont(name: label.font.fontName, size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .green
+        
         view.addSubview(headerView)
+        view.addSubview(label)
         
         // Create the anchors
+        
         headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        
-        /* Ancho y largo van a 1*/
         headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
         headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.20).isActive = true
+        
+        label.centerXAnchor.constraint(equalTo: headerView.centerXAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: headerView.widthAnchor, multiplier: 0.9).isActive = true
+        label.heightAnchor.constraint(equalTo: headerView.heightAnchor, multiplier: 0.20).isActive = true
     }
     
 }
